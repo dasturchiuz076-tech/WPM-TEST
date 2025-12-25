d6724c1265e641ba2402a0ae87ccbb06b1b5a0dd
dasturchiuz076-tech <dasturchiuz076@gmail.com>
2025-12-25 17:47:46 +0500

    Hammasi zo'r!

---
+++ masala.py
@@ -0,0 +1,15 @@
+x = int(input("1-sonni kiriting: "))
+y = int(input("2-sonni kiriting: "))
+amal = input("Amalni kiriting: (+, -, *, ^, /, %): ")
+if amal == "+":
+    print(x + y)
+elif amal == "-":
+    print(x - y)
+elif amal == "*":
+    print(x * y)
+elif amal == "^":
+    print(x ** y)
+elif amal == "/":
+    print(x / y)
+elif amal == "%":
+    print(x % y)
